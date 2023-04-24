816edba84f65a6b2aa51bda0a58670aae5627268
Your Name <you@example.com>
2023-04-24 16:05:45 +0700

    setup sequelize

--- models/LogDatabase.js
+++ models/LogDatabase.js
@@ -1,25 +1,24 @@
 const mongoose = require("mongoose");
 
 const LogDatabaseSchema = new mongoose.Schema({
-  id: String,
-  ipServer: String,
-  hostName: String,
-  pathBackup: String,
-  status: String,
-  osSystems: String,
-  capacityFile: String,
-  DateBackup: String,
-  TimeBackup: String,
-  nameDatabase: String,
-  status: String,
+    id: String,
+    ipServer: String,
+    hostName: String,
+    pathBackup: String,
+    status: String,
+    osSystems: String,
+    capacityFile: String,
+    DateBackup: String,
+    TimeBackup: String,
+    nameDatabase: String,
 });
 
 const LogDatabase = mongoose.model(
-  "LogDatabase",
-  LogDatabaseSchema,
-  "LogDatabase"
+    "LogDatabase",
+    LogDatabaseSchema,
+    "LogDatabase"
 );
 module.exports = {
-  LogDatabaseSchema,
-  LogDatabase,
+    LogDatabaseSchema,
+    LogDatabase,
 };
