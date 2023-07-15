32a25a0d6149660cfe957dc7f4107c1bf31afb0c
DESKTOP-G9SN9IC\admin <bachdangtuan93@gmail.com>
2023-07-15 13:50:31 +0700

    fix lại 1 chút

--- models/virtualmachines.js
+++ models/virtualmachines.js
@@ -7,13 +7,13 @@ module.exports = (sequelize, DataTypes) => {
          * This method is not a part of Sequelize lifecycle.
          * The `models/index` file will call this method automatically.
          */
-        static associate({physicalMachines, servicecontainers}) {
+        static associate({physicalmachines, servicecontainers}) {
             // define association here 1 station thì có nhiều chuyến đi
             this.hasMany(servicecontainers, {
               foreignKey: "belongtoVirtualMachine",
               as: "belongVirtualMachine",
             });
-            this.belongsTo(physicalMachines, {
+            this.belongsTo(physicalmachines, {
               foreignKey: "belongtoPhysicalMachine",
               as: "listVirtualMachine",
             });
